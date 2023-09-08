43566db7ce0fd4af28f6525ae23eba204cc7c5b0
tobirodrigo <tobiasrodrigo2020@gmail.com>
2023-09-08 14:52:30 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,115 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30, 
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+}); 
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30, 
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints : {
+        0: {
+            slidesPerview: 1
+        },
+        520: {
+            slidesPerview: 2
+        },
+        950: {
+            slidesPerview: 3
+        }
+    }
+});
+
+//Carrito 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carrito = document.getElementById("carrito");
+    const elemetos1 = document.getElementById("lista-1");
+    const elemetos2 = document.getElementById("lista-2");
+    const elemetos3 = document.getElementById("lista-3");
+    const lista = document.querySelector("#lista-carrito tbody");
+    const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
+
+    cargarEventListeners();
+
+    function cargarEventListeners() {
+        elemetos1.addEventListener("click", comprarElemento);
+        elemetos2.addEventListener("click", comprarElemento);
+        elemetos3.addEventListener("click", comprarElemento);
+        carrito.addEventListener("click", eliminarElemento);
+
+        vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
+    }
+
+    function comprarElemento(e) {
+        e.preventDefault();
+        if (e.target.classList.contains("agregar-carrito")) {
+            const elemento = e.target.parentElement.parentElement;
+            leerDatosElemento(elemento);
+        }
+    }
+
+    function leerDatosElemento(elemento) {
+        const infoElemento = {
+            imagen: elemento.querySelector("img").src,
+            titulo: elemento.querySelector("h3").textContent,
+            precio: elemento.querySelector(".precio").textContent,
+            id: elemento.querySelector("a").getAttribute("data-ic")
+        }
+
+        insertarCarrito(infoElemento);
+
+    }
+
+    function insertarCarrito(elemento) {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>
+                <img src="${elemento.imagen}" width="100">
+            </td>
+            <td>
+                ${elemento.titulo}
+            </td>
+            <td>
+                ${elemento.precio}
+            </td>
+            <td>
+                <a href="" class="borrar" data-id="${elemento.id}">Borrar</a>
+            </td>
+        `;
+
+        lista.appendChild(row);
+    }
+
+    function eliminarElemento(e) {
+        e.preventDefault();
+        let elemento,
+            elementoId;
+
+        if (e.target.classList.contains("borrar")) {
+            e.target.parentElement.parentElement.remove();
+            elemento = e.target.parentElement.parentElement;
+            elementoId = elemento.querySelector("a").getAttribute("data-id");
+        }
+    }
+
+    function vaciarCarrito() {
+        while (lista.firstChild) {
+            lista.removeChild(lista.firstChild)
+        }
+        return false;
+    }
+});
